a3995c1a44f417e68abff2c221efd11be8cb8aaa
Andrew Diego Beers <andrew.d.beers@gmail.com>
2024-09-24 12:19:29 -0400

    Make a litany of adjustments to hopefully solve the build problem

--- common/src/main/java/org/theomenden/configurablebossbars/client/ConfigurableBossBarsClient.java
+++ common/src/main/java/org/theomenden/configurablebossbars/client/ConfigurableBossBarsClient.java
@@ -6,18 +6,15 @@ import dev.architectury.registry.client.keymappings.KeyMappingRegistry;
 import io.github.jamalam360.jamlib.JamLib;
 import io.github.jamalam360.jamlib.JamLibPlatform;
 import io.github.jamalam360.jamlib.config.ConfigManager;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Environment(EnvType.CLIENT)
 public final class ConfigurableBossBarsClient {
     public static final String MOD_ID = "configurablebossbars";
-    private static final Logger LOGGER = LoggerFactory.getLogger(ConfigurableBossBarsClient.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     public static final ConfigManager<ConfigurableBossBarsConfig> CONFIG = new ConfigManager<>(MOD_ID, ConfigurableBossBarsConfig.class);
     public static KeyMapping toggleBarsKey;
     public static ConfigurableBossBarsConfig configuration;
